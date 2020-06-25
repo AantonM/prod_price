718a7047e39a473eafb41abfa305c6aa04ca3093
Anton <AantonM@users.noreply.github.com>
2020-06-25 21:21:41 +0100

    Initial commit

---
+++ src/main/java/com/prodprice/prodprice_service/ProdPriceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.prodprice.prodprice_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdPriceServiceApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ProdPriceServiceApplication.class);
+    }
+}
